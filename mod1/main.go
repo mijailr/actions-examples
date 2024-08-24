6d4aaaf4a4ede0821a6bfd1ba595a87541df355e
Mijail Rondon <mijail@riseup.net>
2024-08-24 09:35:02 -0500

    add mod example

---
+++ mod1/main.go
@@ -0,0 +1,17 @@
+// main example
+
+package mod1
+
+// hello world
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+
+type Mod1 struct {
+	// mod1
+	state string
+}
---
+++ mod2/main.go
@@ -0,0 +1,16 @@
+// main example
+
+package mod2
+
+// hello world
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+type Mod2 struct {
+	// mod1
+	state string
+}
